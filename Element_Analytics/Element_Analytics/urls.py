9651ba2492ac296f90e0a53e38f9f4304ddbbe14
Ly Nguyen <yl.neyugn@gmail.com>
2018-02-17 21:59:00 -0800

    adds main view, restructures project

--- Element_Analytics/Element_Analytics/urls.py
+++ Element_Analytics/Element_Analytics/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='index/')),
     path('admin/', admin.site.urls),
+    path('index/', include('apps.index.urls'))
 ]
---
+++ Element_Analytics/apps/index/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.shortcuts import loader
+from django.views import View
+# Create your views here.
+
+
+class MainView(View):
+    def get(self, request, *args, **kwargs):
+        page = loader.get_template('index.html')
+        return HttpResponse(page.render())
+
