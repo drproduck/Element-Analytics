b84a53a18f459f948e35df75831535d98f5e87d4
drproduck <duckhiem95@yahoo.com>
2018-02-18 19:24:54 -0800

    added comments to settings URL_ROOT (fixed)

--- Element_Analytics/Element_Analytics/settings.py
+++ Element_Analytics/Element_Analytics/settings.py
@@ -35,13 +35,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'analytics.apps.AnalyticsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'apps.analytics',
+    'apps.core'
+
 ]
 
 MIDDLEWARE = [
@@ -124,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
