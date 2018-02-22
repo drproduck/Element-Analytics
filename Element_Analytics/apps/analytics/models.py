feeeccf0b83a7a6bcfb4aa60e25b83ad83c0b11a
WeimengPu <weimeng.pu@foxmail.com>
2018-02-21 16:08:07 -0800

    Move the classes in analytics/models.py into core/models.py

--- Element_Analytics/apps/analytics/models.py
+++ Element_Analytics/apps/analytics/models.py
@@ -3,23 +3,3 @@ from django.forms import ModelForm
 
 
 # Create your models here.
-class User(models.Model):
-    """
-    user model containing one user's information
-    """
-
-    def __str__(self):
-        return self.user_name
-
-    user_name = models.CharField(max_length=200)
-    pwd = models.CharField(max_length=200)
-    dob = models.DateTimeField()
-    date_joined = models.DateTimeField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True)
-    email = models.EmailField(max_length=200)
-
-
-class UserForm(ModelForm):
-    class Meta:
-        model = User
-        fields = ['user_name', 'dob', 'email', ]
--- Element_Analytics/apps/core/models.py
+++ Element_Analytics/apps/core/models.py
@@ -3,10 +3,32 @@ from __future__ import unicode_literals
 from django.db import models
 import os
 
+class User(models.Model):
+    """
+    user model containing one user's information
+    """
+
+    def __str__(self):
+        return self.user_name
+
+    user_name = models.CharField(max_length=200)
+    pwd = models.CharField(max_length=200)
+    dob = models.DateTimeField()
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    email = models.EmailField(max_length=200)
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['user_name', 'dob', 'email', ]
 
 class Document(models.Model):
     def filename(self):
         return os.path.basename(self.document.name)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.CharField(max_length=255, blank=True)
     document = models.FileField(upload_to='documents/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
