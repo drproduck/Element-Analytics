f23cd367164336414d42820efd90115d32ebccbf
Cole Sakamoto <cole_sakamoto@yahoo.com>
2018-02-27 11:46:09 -0800

    comment out simple upload url

--- Element_Analytics/apps/core/urls.py
+++ Element_Analytics/apps/core/urls.py
@@ -4,6 +4,6 @@ from apps.core import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^simple/$', views.simple_upload, name='simple_upload'),
+    #url(r'^simple/$', views.simple_upload, name='simple_upload'),
     url(r'^form/$', views.model_form_upload, name='model_form_upload'),
 ]
