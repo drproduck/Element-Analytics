710769fb178cca17c48022053eba5d28381b0413
Luis Pamintuan <lu.pamintuan@yahoo.com>
2018-03-01 00:35:55 -0800

    Log in and Sign up added
    Log in and Sign up tabs now updated and working
    
    Index.html changed to new url

--- Element_Analytics/apps/login/urls.py
+++ Element_Analytics/apps/login/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.conf.urls import url
 from django.contrib.auth import views
+from apps.login.core import views as core_views
 
 urlpatterns = [
-    path('', views.login, name='login')
-
+    path('', views.login, name='login'),
+    url(r'^signup/$', core_views.signup, name='signup'),
 ]
