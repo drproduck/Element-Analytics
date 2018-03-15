2c2d6eb2b5c130f3850e9a8f04cde7d34e0bd6b9
Ly Nguyen <yl.neyugn@gmail.com>
2018-03-15 01:26:26 +0000

    added function parse_list_of_dicts so we can insert to the database more conveniently

--- Element_Analytics/Tests/unit_tests/parsers/test_logparser.py
+++ Element_Analytics/Tests/unit_tests/parsers/test_logparser.py
@@ -4,12 +4,18 @@ import time
 
 #Initial test. Performance ~ 24s for 100,000 log entries
 
-pp = pprint.PrettyPrinter(indent=4)
+pp = pprint.PrettyPrinter(indent=4, compact=True)
 
 file = "./test_log"
+
 start_time = time.time()
-res = parser.parse(file)
+# res = parser.parse_dict_of_lists(file)
+res = parser.parse_list_of_dicts(file)
 elapsed_time = time.time() - start_time
+
 print("Elapse time: ", elapsed_time)
-pp.pprint(res)
+
+for item in res:
+    pp.pprint(item)
+    print()
 
--- Element_Analytics/libs/parser/logparser.py
+++ Element_Analytics/libs/parser/logparser.py
@@ -1,7 +1,7 @@
 """This module handles log file by parsing input logs
 and return a condensed version of the logs with nonessential
 information removed. The return data structures can be either
-a dictionary or a dataframe supported by Pandas library"""
+a list of entries or a dataframe supported by Pandas library"""
 
 from libs.parser.tokenizer import LogTokenizer
 import os.path
@@ -35,9 +35,8 @@ def __parse_line(line):
     return tokens
 
 
-
 def __append_item(log_dict, token_list):
-    """helper: append items into the dictionary"""
+    """helper: extract items into the dictionary"""
     try:
         log_dict[DATE].append(token_list[DATE])
         log_dict[NAME].append(token_list[NAME])
@@ -48,8 +47,10 @@ def __append_item(log_dict, token_list):
         print(e)
         exit()
 
-def parse(path_to_file):
-    """parsing the log file and return a dictionary representation"""
+
+def parse_dict_of_lists(path_to_file):
+    """parsing the log file and return a
+    list of dictionaries representation"""
     __validate_path(path_to_file)
     log_dict = {
         DATE : [],
@@ -68,3 +69,17 @@ def parse(path_to_file):
         exit()
 
     return log_dict
+
+
+def parse_list_of_dicts(path_to_file):
+    """parse to list of dicts instead of dict of list"""
+    __validate_path(path_to_file)
+    list = []
+    try:
+        with open(path_to_file, 'r') as f:
+            for l in f:
+                list.append( __parse_line(l))
+    except IOError as e:
+        print("Couldn't read file. Error: ", e)
+        exit()
+    return list
