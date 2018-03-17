380d6009ebf6750215c3e8fd093746e5de310bef
Ly Nguyen <yl.neyugn@gmail.com>
2018-03-17 21:34:32 +0000

    added test for log parser

--- Element_Analytics/Tests/unit_tests/parsers/test_logparser.py
+++ Element_Analytics/Tests/unit_tests/parsers/test_logparser.py
@@ -1,21 +1,54 @@
 import libs.parser.logparser as parser
-import pprint
-import time
+import libs.parser.logfields as lf
+import unittest
 
-#Initial test. Performance ~ 24s for 100,000 log entries
 
-pp = pprint.PrettyPrinter(indent=4, compact=True)
+with open("./test_log", 'r') as f:
+    input = [line for line in f]
 
-file = "./test_log"
+golden = [
+    {
+        lf.DATE : "Aug 20 06:38:51",
+        lf.NAME : "dchq_tomcat[38866]",
+        lf.TYPE : "INFO",
+        lf.INFO : "s.m.r.HostPingNotificationMessageHandler",
+        lf.MSSG : "Received server ping for [13bb3a25-0b37-4167-b2eb-476720ebb0a5]"
+    },
+    {
+        lf.DATE : "Aug 20 06:39:02",
+        lf.NAME : "dchq_solr[38866]",
+        lf.TYPE : None,
+        lf.INFO : None,
+        lf.MSSG : "011commit{dir=NRTCachingDirectory(MMapDirectory@/opt/solr-4.10.4/example/solr/collection1/data/index lockFactory=NativeFSLockFactory@/opt/solr-4.10.4/example/solr/collection1/data/index; maxCacheMB=48.0 maxMergeSizeMB=4.0),segFN=segments_7d8f,generation=343743}"
+    },
+    {
+        lf.DATE : "Aug 20 06:39:02",
+        lf.NAME : "dchq_solr[38866]",
+        lf.TYPE : "INFO",
+        lf.INFO : "org.apache.solr.update.processor.LogUpdateProcessor",
+        lf.MSSG : "[collection1] webapp=/solr path=/update params={waitSearcher=true&commit=true&softCommit=false&wt=javabin&version=2} {commit=} 0 81"
+    },
+    {
+        lf.DATE : "Aug 20 06:59:22",
+        lf.NAME : "dchq_nginx[38866]",
+        lf.TYPE : None,
+        lf.INFO : "164.132.91.13",
+        lf.MSSG : '[20/Aug/2017:13:59:22 +0000] "GET / HTTP/1.1" 302 0 "-" "Firefox/24.0" "-"#015'
+    }
+]
 
-start_time = time.time()
-# res = parser.parse_dict_of_lists(file)
-res = parser.parse_list_of_dicts(file)
-elapsed_time = time.time() - start_time
 
-print("Elapse time: ", elapsed_time)
+class Test_Parser(unittest.TestCase):
 
-for item in res:
-    pp.pprint(item)
-    print()
+    def test_parseline(self):
+        self.assertEqual(parser.parse_line(input[0]), golden[0])
+        self.assertEqual(parser.parse_line(input[1]), golden[1])
+        self.assertEqual(parser.parse_line(input[2]), golden[2])
+        self.assertEqual(parser.parse_line(input[3]), golden[3])
 
+    def test_parsefile(self):
+        self.assertEqual(parser.parse_file("./test_log"), golden)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- Element_Analytics/libs/parser/logparser.py
+++ Element_Analytics/libs/parser/logparser.py
@@ -8,17 +8,17 @@ import os.path
 import libs.parser.logfields as field
 
 
-__tokenizer = LogTokenizer().build()
+tokenizer = LogTokenizer().build()
 
 
-def __validate_path(path_to_file):
+def validate_path(path_to_file):
     """check if path is valid"""
     if not os.path.isfile(path_to_file):
         print("File does not exist. Exit")
         exit()
 
 
-def __parse_line(line):
+def parse_line(line):
     """parse a single line of log"""
     tokens = {
         field.DATE : None,
@@ -27,56 +27,20 @@ def __parse_line(line):
         field.INFO : None,
         field.MSSG : None
     }
-    __tokenizer.input(line)
-    for tok in __tokenizer:
+    tokenizer.input(line)
+    for tok in tokenizer:
         tokens[tok.type] = tok.value
     return tokens
 
 
-def __append_item(log_dict, token_list):
-    """helper: extract items into the dictionary"""
-    try:
-        log_dict[field.DATE].append(token_list[field.DATE])
-        log_dict[field.NAME].append(token_list[field.NAME])
-        log_dict[field.TYPE].append(token_list[field.TYPE])
-        log_dict[field.INFO].append(token_list[field.INFO])
-        log_dict[field.MSSG].append(token_list[field.MSSG])
-    except LookupError as e:
-        print(e)
-        exit()
-
-
-def parse_dict_of_lists(path_to_file):
-    """parsing the log file and return a
-    list of dictionaries representation"""
-    __validate_path(path_to_file)
-    log_dict = {
-        field.DATE : [],
-        field.NAME : [],
-        field.TYPE : [],
-        field.INFO : [],
-        field.MSSG : []
-    }
-    try:
-        with open(path_to_file, 'r') as f:
-            for l in f:
-                token_list = __parse_line(l)
-                __append_item(log_dict, token_list)
-    except IOError as e:
-        print("Couldn't read file. Error: ", e)
-        exit()
-
-    return log_dict
-
-
-def parse_list_of_dicts(path_to_file):
-    """parse to list of dicts instead of dict of list"""
-    __validate_path(path_to_file)
+def parse_file(path_to_file):
+    """parse a log file into a list of log entries in dictionaries"""
+    validate_path(path_to_file)
     list = []
     try:
         with open(path_to_file, 'r') as f:
             for l in f:
-                list.append( __parse_line(l))
+                list.append(parse_line(l))
     except IOError as e:
         print("Couldn't read file. Error: ", e)
         exit()
