29d3b46509722280f54af313bf9c446e47fc76e8
Cole Sakamoto <cole_sakamoto@yahoo.com>
2018-03-20 00:13:36 -0700

    renamed file upload description

--- Element_Analytics/apps/upload/forms.py
+++ Element_Analytics/apps/upload/forms.py
@@ -6,4 +6,4 @@ from apps.upload.models import Document
 class DocumentForm(forms.ModelForm):
     class Meta:
         model = Document
-        fields = ('document', 'description',)
+        fields = ('document', 'file_name',)
