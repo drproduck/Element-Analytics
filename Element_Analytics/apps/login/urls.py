59a4e0a779338d686215f24b6f302f7eee93d0c4
drproduck <duckhiem95@yahoo.com>
2018-03-24 23:04:03 -0700

    implement log out view add userbase which is base template for user-specific pages restrict access to dashboard: dashboard now only accessible if logged in, else redirect to log in page
    NOTE: please use base and userbase extensively and appropriately to avoid repeat code

--- Element_Analytics/apps/login/urls.py
+++ Element_Analytics/apps/login/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from django.contrib.auth import views
 
+app_name = 'login'
 urlpatterns = [
     path('', views.LoginView.as_view(template_name='login/login.html'), name='login'),
+    path('logout', views.LogoutView.as_view(template_name='login:logout', next_page='main'), name='logout')
 ]
