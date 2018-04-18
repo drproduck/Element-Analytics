02c4ffbf4ad0cb2bfc7337e752ab7cd7befb4901
Ly Nguyen <yl.neyugn@gmail.com>
2018-04-18 00:04:46 +0000

    generic tokenizer

---
+++ Element_Analytics/libs/parser/generic_tokenizer.py
@@ -0,0 +1,10 @@
+"""
+This class represents a generic tokenizer
+that can takes a dictionary of fields and
+regular expressions to tokenize text instead
+of hard coding
+"""
+
+class GenericTokenizer(object):
+
+    def __init__(selfs, ):
