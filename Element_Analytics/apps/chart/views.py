ddeb56e9cbaa7c69bc532e643701791c30057429
WeimengPu <weimeng.pu@foxmail.com>
2018-04-29 20:35:39 -0700

    Pie added

--- Element_Analytics/apps/chart/views.py
+++ Element_Analytics/apps/chart/views.py
@@ -11,15 +11,22 @@ from .json2chart import json2chart
 # please go to the samples folder for others view
 
 def chart(request):
-    str_keyword, str_date = json2chart();
+    str_keyword, str_date, str_keyword_pie, str_date_pie = json2chart();
     # Create an object for the column2d chart using the FusionCharts class constructor
-    column2d_keyword = FusionCharts("column2d", "ex1" , "750", "350", "chart-1", "json",
+    column2d_keyword = FusionCharts("column2d", "id0" , "700", "350", "chart-1", "json",
         # The data is passed as a string in the `dataSource` as parameter.
     str_keyword)
 
-    column2d_date = FusionCharts("column2d", "ex2" , "750", "350", "chart-2", "json",
+    column2d_date = FusionCharts("column2d", "id1" , "700", "350", "chart-2", "json",
         # The data is passed as a string in the `dataSource` as parameter.
     str_date)
 
+    pie3d_keyword = FusionCharts("pie3d", "id2" , "700", "350", "chart-3", "json", str_keyword_pie)
+
+    pie3d_date = FusionCharts("pie3d", "id3" , "700", "350", "chart-4", "json", str_date_pie)
+
     # returning complete JavaScript and HTML code, which is used to generate chart in the browsers.
-    return render(request, 'chart/index.html', {'output_keyword' : column2d_keyword.render(), 'output_date' : column2d_date.render()})
+    return render(request, 'chart/index.html',
+        {'output_keyword' : column2d_keyword.render(), 'output_date' : column2d_date.render(),
+        'output_keyword_pie' : pie3d_keyword.render(), 'output_date_pie' : pie3d_date.render()
+        })
