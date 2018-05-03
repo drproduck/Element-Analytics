aaf17be9fe54add68dced5b14632007792400dcc
Cole Sakamoto <cole_sakamoto@yahoo.com>
2018-05-03 11:42:26 -0700

    Fix change user name bug

--- Element_Analytics/apps/edit/forms.py
+++ Element_Analytics/apps/edit/forms.py
@@ -6,7 +6,6 @@ class EditProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = (
-            'username',
             'email',
             'first_name',
             'last_name',
