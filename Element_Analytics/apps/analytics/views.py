29a2a83d6fb7c7374083104fc501053c35ca50e2
Ly Nguyen <yl.neyugn@gmail.com>
2018-05-04 21:04:41 -0700

    wip

--- Element_Analytics/apps/analytics/views.py
+++ Element_Analytics/apps/analytics/views.py
@@ -1,33 +1,31 @@
+""" Analytics view module """
+import os
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+
+import libs.utilities.pathtools  as pt
 from Element_Analytics.settings import MEDIA_URL
 from Element_Analytics.settings import DOCUMENT_ROOT, BASE_DIR
-from ..upload.models import LogFile
-import libs.utilities.pathtools  as pt
 
-import os
-import pandas as pd
-
-from django.contrib.auth.decorators import login_required
 
 STATIC_DIR = os.path.join(BASE_DIR, 'apps/analytics/static/analytics/')
 
 
 def get_user_log_dir(user,  log):
-     """root -> user dir -> log dir -> bunch of .csv and a single .raw"""
-     return pt.get_log_dir_abs(user, log ) + ".csv"
+    """root -> user dir -> log dir -> bunch of .csv and a single .raw"""
+    return pt.get_log_dir_abs(user, log ) + ".csv"
 
 
 @login_required
-def MainView(request, log_name):
-     user = request.user
-     file_path = os.path.join(pt.get_log_dir_abs(user.username, log_name), log_name + ".csv")
-     logfile = user.logfile_set.get(log_name=log_name)
-     if not logfile.regex:
-          return render(request, "analytics/mainpage.djt", {"log_name" : log_name})
-     with open(file_path, 'r') as fp:
-          response = HttpResponse(fp.read(), content_type='text/csv')
-          response['Content-Disposition'] = 'attachment; filename=' + log_name + ".csv"
-          return response
-     
-
+def main_view(request, log_name):
+    """ Analytics main view """
+    user = request.user
+    file_path = os.path.join(pt.get_log_dir_abs(user.username, log_name), log_name + ".csv")
+    logfile = user.logfile_set.get(log_name=log_name)
+    if not logfile.regex:
+        return render(request, "analytics/mainpage.djt", {"log_name" : log_name})
+    with open(file_path, 'r') as fp:
+        response = HttpResponse(fp.read(), content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=' + log_name + ".csv"
+        return response
--- Element_Analytics/libs/analytics/analytics.py
+++ Element_Analytics/libs/analytics/analytics.py
@@ -24,52 +24,51 @@ COMMON_ERROR_KEYS = [
 
 
 def error_analytics(dataframe):
-     """ Return a JSON object contains insight
-     about the errors of this log file"""
-     frames = []
-     res_dict = {}
-     for k in COMMON_ERROR_KEYS:
-          frames.append(dataframe[dataframe.message.str.contains(pat=k, case=False) == True])     
-     result = pd.concat(frames)
-     res_dict['total_entries'] = len(dataframe.index)
-     res_dict['num_error'] = len(result.index)
-     res_dict['error_rate'] = res_dict['num_error'] / res_dict['total_entries']
-     res_dict['error_by_keywords'] = count_error_occurences(result)
-     res_dict['error_by_dates'] = result.groupby('date').date.size().to_dict()
-     return json.dumps(res_dict, cls=encoder, indent=4, sort_keys=True)
+    """ Return a JSON object contains insight
+    about the errors of this log file"""
+    frames = []
+    res_dict = {}
+    for k in COMMON_ERROR_KEYS:
+        frames.append(dataframe[dataframe.message.str.contains(pat=k, case=False) == True])     
+    result = pd.concat(frames)
+    res_dict['total_entries'] = len(dataframe.index)
+    res_dict['num_error'] = len(result.index)
+    res_dict['error_rate'] = res_dict['num_error'] / res_dict['total_entries']
+    res_dict['error_by_keywords'] = count_error_occurences(result)
+    res_dict['error_by_dates'] = result.groupby('date').date.size().to_dict()
+    return json.dumps(res_dict, cls=encoder, indent=4, sort_keys=True)
 
 
 def user_analytics(user):
-     """ Return JSON contains user information"""
-     res_dict = {}
-     res_dict['username'] = user.username
-     res_dict['num_log_limit'] = settings.NUM_LOG_LIMIT
-     res_dict['num_log'] = user.logfile_set.count()
-     res_dict['storage_limit'] = settings.STORAGE_LIMIT
-     res_dict['storage_used'] = pt.get_user_dir_size(user.username)
-     return json.dumps(res_dict, indent=4, sort_keys=True)
+    """ Return JSON contains user information"""
+    res_dict = {}
+    res_dict['username'] = user.username
+    res_dict['num_log_limit'] = settings.NUM_LOG_LIMIT
+    res_dict['num_log'] = user.logfile_set.count()
+    res_dict['storage_limit'] = settings.STORAGE_LIMIT
+    res_dict['storage_used'] = pt.get_user_dir_size(user.username)
+    return json.dumps(res_dict, indent=4, sort_keys=True)
      
 
 def count_error_occurences(dataframe):
-     error_dict = {}
-     for key in COMMON_ERROR_KEYS:
-          for row in dataframe[(dataframe.message.str.contains(key))].count():
-               error_dict[key] = row
-     return error_dict
+    error_dict = {}
+    for key in COMMON_ERROR_KEYS:
+        for row in dataframe[(dataframe.message.str.contains(key))].count():
+            error_dict[key] = row
+    return error_dict
 
 
 class encoder(json.JSONEncoder):
-     """ Customized encoder to encode numpy types"""
-     def default(self, obj):
-          if isinstance(obj, numpy.integer):
-               return int(obj)
-          elif isinstance(obj, numpy.floating):
-               return float(obj)
-          elif isinstance(obj, numpy.ndarray):
-               return obj.tolist()
-          else:
-               return super(encoder, self).default(obj)
-
+    """ Customized encoder to encode numpy types"""
+    def default(self, obj):
+        if isinstance(obj, numpy.integer):
+            return int(obj)
+        elif isinstance(obj, numpy.floating):
+            return float(obj)
+        elif isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        else:
+            return super(encoder, self).default(obj)
 
 # start_time = time.process_time()
 # log = lp.read_log("ly-admin", "syslogClassShare.5")
